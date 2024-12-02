4a772770917b38608fb267e497fe15fbaf8702b5
Filippo Pesavento <pesaventofilippo@gmail.com>
2024-12-02 10:40:29 +0100

    Added 2024/day02

--- 2024/day02/main.py
+++ 2024/day02/main.py
@@ -1,9 +1,28 @@
+def is_safe(levels: list[int]) -> bool:
+    is_increasing = all(levels[i] < levels[i + 1] for i in range(len(levels) - 1))
+    is_decreasing = all(levels[i] > levels[i + 1] for i in range(len(levels) - 1))
+    abs_check = all(1 <= abs(levels[i] - levels[i + 1]) <= 3 for i in range(len(levels) - 1))
+    return (is_decreasing or is_increasing) and abs_check
+
+
 def part1(lines: list[str]) -> int:
-    pass
+    return sum(is_safe([int(x) for x in report.split()]) for report in lines)
 
 
 def part2(lines: list[str]) -> int:
-    pass
+    total = 0
+    for report in lines:
+        levels = [int(x) for x in report.split()]
+        if is_safe(levels):
+            total += 1
+        else:
+            for i in range(len(levels)):
+                new_levels = levels.copy()
+                new_levels.pop(i)
+                if is_safe(new_levels):
+                    total += 1
+                    break
+    return total
 
 
 def main():
