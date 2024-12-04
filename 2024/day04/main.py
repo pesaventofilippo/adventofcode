03dc7600f0d73862f85aefed5c571b1a1cb53fcd
Filippo Pesavento <pesaventofilippo@gmail.com>
2024-12-04 11:35:27 +0100

    Added 2024/day04

--- 2024/day04/main.py
+++ 2024/day04/main.py
@@ -1,9 +1,69 @@
 def part1(lines: list[str]) -> int:
-    pass
+    def find_xmas(x: int, y: int, dir: str, step: int=0) -> bool:
+        if y < 0 or y >= len(lines) or x < 0 or x >= len(lines[y]):
+            return False
+
+        letter = "XMAS"[step]
+        if lines[y][x] != letter:
+            return False
+        if step == 3:
+            return True
+
+        if "N" in dir:
+            y -= 1
+        if "E" in dir:
+            x += 1
+        if "S" in dir:
+            y += 1
+        if "W" in dir:
+            x -= 1
+
+        return find_xmas(x, y, dir, step+1)
+
+    total = 0
+    for y in range(len(lines)):
+        for x in range(len(lines[y])):
+            for dir in ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]:
+                total += find_xmas(x, y, dir)
+    return total
 
 
 def part2(lines: list[str]) -> int:
-    pass
+    centers: list[tuple[int, int]] = []
+
+    def find_xmas(x: int, y: int, dir: str, step: int = 0, center: tuple[int, int] = None) -> bool:
+        if y < 0 or y >= len(lines) or x < 0 or x >= len(lines[y]):
+            return False
+
+        letter = "MAS"[step]
+        if lines[y][x] != letter:
+            return False
+        if step == 1:
+            center = (x, y)
+        if step == 2:
+            if center in centers:
+                return True
+            else:
+                centers.append(center)
+                return False
+
+        if "N" in dir:
+            y -= 1
+        if "E" in dir:
+            x += 1
+        if "S" in dir:
+            y += 1
+        if "W" in dir:
+            x -= 1
+
+        return find_xmas(x, y, dir, step+1, center)
+
+    total = 0
+    for y in range(len(lines)):
+        for x in range(len(lines[y])):
+            for dir in ["NE", "SE", "SW", "NW"]:
+                total += find_xmas(x, y, dir)
+    return total
 
 
 def main():
